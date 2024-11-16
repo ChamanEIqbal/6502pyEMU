cfd7d29539f99a26fc20e95b51408621d85427c3
ChamanEIqbal <pc19232.mustafa@gmail.com>
2024-11-16 13:05:35 +0500

    update: emu.py

--- emu.py
+++ emu.py
@@ -7,6 +7,8 @@ Word = NewType('Word', int)
 u32 = NewType('u32', int)
 s32 = NewType('s32', int)
 
+# this feels very bad
+
 class Mem: # memory class
     MAX_MEM : u32 = 1024 * 64 # 64 kilobytes of memory
     
@@ -17,12 +19,12 @@ class Mem: # memory class
     def resetMem(self):
         self.Data = [Byte(0)] * self.MAX_MEM
     
-    def __getitem__(self, address: u32) -> Byte:
+    def __getitem__(self, address: Word) -> Byte:
         if(address >= self.MAX_MEM):
             raise IndexError(f"Address {address} is out of range...")
         return self.Data[address]
     
-    def __setitem__(self, address : u32, value : Byte) -> None:
+    def __setitem__(self, address : Word, value : Byte) -> None:
         if address >= self.MAX_MEM:
             raise IndexError(f"Address {address} is out of range...")
         if not (0 <= value <= 255):
@@ -59,7 +61,7 @@ class StatusFlags: # status flags, little endian (C LSB, N MSB) [8 bits, 1 byte]
 
 
 class CPU:
-    def __init__(self):    
+    def __init__(self, mem : Mem):    
         self.PC : Word = Word(0) # program counter
         self.SP : Byte  = Byte(0) # stack pointer
 
@@ -68,7 +70,9 @@ class CPU:
         self.Y : Byte = Byte(0) # register Y
 
         self.statusFlags = StatusFlags() # status flags
-    
+        self.mem = Mem()
+        self.cycles_consumed : s32 = 0
+              
 
     @property
     def PS(self) -> Byte: # processor status (status flags to byte as cpu property (cpu.PS))
@@ -78,6 +82,14 @@ class CPU:
     def PS(self, p_byte : Byte):
         self.statusFlags.from_byte(p_byte)
 
+    
+    def fetchByte(self, address : Word) -> Byte:
+        self.cycles_consumed+=1
+        return self.mem[address]
+
+    def writeByte(self, address : Word, data : Byte):
+        self.cycles_consumed+=1
+        self.mem[address] =  data
 
 
     # The reset routine of 6502 takes 7 cycles, and starts from 0xFFFC (reset vector address afterwards...)
@@ -86,3 +98,12 @@ class CPU:
 
 
     ## WIP
+
+
+if __name__ == "__main__":
+    mem = Mem() # can be loaded with .bin files (pre configured memories, emulated BIOS etc)
+    cpu = CPU(mem)
+
+    cpu.writeByte(0xFFFC, 0xA9) # this should not be done this way, this should be done through Instruction Sets
+    print(hex(cpu.fetchByte(0xFFFC))) # expected : 0xa9, test passed
+    print(f"no. of cycles consumed throughout: {cpu.cycles_consumed}") # expected : 2 cycles
