3a1b708930d1e58b8896e1f67bdd1a32128de27b
ChamanEIqbal <pc19232.mustafa@gmail.com>
2025-01-01 15:42:47 +0500

    docs linesplitter v0.0.2

--- gui/linesplitter.py
+++ gui/linesplitter.py
@@ -2,10 +2,18 @@ class LineSplitter: # This module will use the 2 pass assembler!
     @staticmethod
     def split_lines(text):
         """
+        6502 Emulator - v0.0.2: LINE SPLITTER MODULE
         Splits the given text into lines and prints each line.
         
         Args:
             text (str): The input text to split into lines.
+
+        Work Flow Function (*) ==> THIS:
+            -*Gives to API, JSON request.
+            -API Manages / Assembles the request and returns a JSON response.
+            -*Gets Response from API, prints the Assembled Code to Console.
+            -Responsed API is put to Memory of CPU at backend.
+            -Backend Memory uses Program for Execution / Steps for Debugger or PPU.
         """
         lines = text.splitlines()
         print("Split lines:")
